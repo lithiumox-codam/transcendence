b4e72dea554dfc610fd275e3e676c0d0e15adae4
Mees Dekker <lithium@Mees-MBP.localdomain>
2025-01-17 01:06:28 +0100

    Added static file serving. Used Caddy to serve the static content generated by Django and middleware for correction of redirect urls.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "backend.middleware.api_prefix_middleware.ApiPrefixMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -113,9 +114,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "api/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
+
+FORCE_SCRIPT_NAME = '/api'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True

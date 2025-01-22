3c20565b0a53edad57c192bc0b18edf538cfb1a0
Vincent Ockers <vincentbockers@gmail.com>
2025-01-22 18:32:15 +0100

    Fix saving password on signup

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["id", "username", "email", "password"]
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
 
     def validate(self, data):
         if User.objects.filter(username__iexact=data["username"]).exists():

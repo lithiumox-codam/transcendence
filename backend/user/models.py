d9d65c6b08e80593c0fd6d6ac3f30275b7414bbd
Vincent Ockers <vincentbockers@gmail.com>
2025-01-22 19:34:46 +0100

    Add avatar upload for user

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
     def __str__(self):
         return self.username
 

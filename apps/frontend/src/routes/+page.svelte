ce9c3863f2e1999cd28d75ca2a5f177bf35ed6c4
LithiumOx <me@meesdekker.xyz>
2025-02-05 14:54:31 +0100

    Made it so formatting is the same in svelte files as every other file. Biome is now the linter for everything typescript but not the formatter for svelte file as it just does not work properly. The speed of linting is so good that I just dont want to revert back to eslint.
    (Also started work on Drizzle as a seperate package!)

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-let test = $state<string>("test");
+    let test = $state<string>("test");
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="p-5">Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
 <p>
     {test}
 </p>

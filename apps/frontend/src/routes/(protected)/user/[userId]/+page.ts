b78cb15ef063e4788fdbc1fe398b5e3bdf24c0c3
Mees Dekker <me@meesdekker.xyz>
2025-02-20 17:43:42 +0100

    Enhance user profile display; update user loading logic and remove unused imports

--- apps/frontend/src/routes/(protected)/user/[userId]/+page.ts
+++ apps/frontend/src/routes/(protected)/user/[userId]/+page.ts
@@ -1,9 +1,10 @@
 import { client } from "$lib/trpc";
 import type { PageLoad } from "./$types";
 
-export const load = (async () => {
+export const load = (async ({ params }) => {
+    const user = await client.user.getById.query(Number(params.userId));
     return {
-        user: await client.user.get.query(),
+        user: user[0],
         friends: await client.user.friends.list.query(),
         requestsOut: await client.user.friends.listRequests.query(),
         requestsIn: await client.user.friends.listSentRequests.query(),
--- packages/trpc/src/router/user.ts
+++ packages/trpc/src/router/user.ts
@@ -1,6 +1,5 @@
 import {
     type Friend,
-    type User,
     db,
     friends,
     userInputSchema,

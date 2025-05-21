36f7d444f9786014162380dcddd2fe615308e0ba
Mees Dekker <me@meesdekker.xyz>
2025-05-21 14:20:11 +0200

    refactor Making it more shitty for the Codam linux restrictions

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,18 +1,16 @@
 import fastify from "./app";
 
 const app = fastify({
-    pluginTimeout: 50000,
-    bodyLimit: 15485760,
+	pluginTimeout: 50000,
+	bodyLimit: 15485760,
 });
 
 try {
-    app.listen({ port: import.meta.env.PROD ? 8000 : 8080, host: "0.0.0.0" });
-    console.log("Server listening on port", import.meta.env.PROD ? 8000 : 8080);
-    console.log(process.env);
-    console.log("Environment:", import.meta.env);
+	app.listen({ port: import.meta.env.PROD ? 8000 : 8080, host: "0.0.0.0" });
+	console.log("Server listening on port", import.meta.env.PROD ? 8000 : 8080);
 } catch (err) {
-    app.log.error(err);
-    process.exit(1);
+	app.log.error(err);
+	process.exit(1);
 }
 
 export const backend = app;
--- packages/auth/src/oauth.ts
+++ packages/auth/src/oauth.ts
@@ -1,32 +1,32 @@
 export class TokenPayload {
-    grant_type: string;
-    scope: string;
-    code: string;
-    client_id: string;
-    client_secret: string;
-    redirect_uri: string;
+	grant_type: string;
+	scope: string;
+	code: string;
+	client_id: string;
+	client_secret: string;
+	redirect_uri: string;
 
-    constructor(client_id: string, client_secret: string, code: string) {
-        this.grant_type = "authorization_code";
-        this.code = code;
-        this.scope = "openid profile email";
-        this.client_id = client_id;
-        this.client_secret = client_secret;
-        this.redirect_uri = `https://${process.env.VITE_HOSTNAME}/oauth/callback`;
-    }
+	constructor(client_id: string, client_secret: string, code: string) {
+		this.grant_type = "authorization_code";
+		this.code = code;
+		this.scope = "openid profile email";
+		this.client_id = client_id;
+		this.client_secret = client_secret;
+		this.redirect_uri = import.meta.env.PROD ? `https://${process.env.VITE_HOSTNAME}:8080/oauth/callback` : `http://localhost/oauth/callback`;
+	}
 
-    /**
-     * Converts the token payload to a query string.
-     * @returns The query string.
-     */
-    toQueryString(): string {
-        return new URLSearchParams({
-            grant_type: this.grant_type,
-            scope: this.scope,
-            code: this.code,
-            client_id: this.client_id,
-            client_secret: this.client_secret,
-            redirect_uri: this.redirect_uri,
-        }).toString();
-    }
+	/**
+	 * Converts the token payload to a query string.
+	 * @returns The query string.
+	 */
+	toQueryString(): string {
+		return new URLSearchParams({
+			grant_type: this.grant_type,
+			scope: this.scope,
+			code: this.code,
+			client_id: this.client_id,
+			client_secret: this.client_secret,
+			redirect_uri: this.redirect_uri,
+		}).toString();
+	}
 }
